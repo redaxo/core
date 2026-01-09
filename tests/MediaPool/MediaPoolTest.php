d4022fd9a0d1c7eae5a70c73b459006b81e4950d
Gregor Harlan <mail@gh01.de>
2026-01-09 18:06:33 +0100

    Core-Changelog von R5 entfernt

--- tests/MediaPool/MediaPoolTest.php
+++ tests/MediaPool/MediaPoolTest.php
@@ -56,8 +56,8 @@ final class MediaPoolTest extends TestCase
         return [
             [false, __FILE__],
             [false, __FILE__, 'foo.md'],
-            [true, __DIR__ . '/../../redaxo/src/core/CHANGELOG.md'],
-            [false, __DIR__ . '/../../redaxo/src/core/CHANGELOG.md', 'foo.txt'],
+            [true, __DIR__ . '/../../README.md'],
+            [false, __DIR__ . '/../../README.md', 'foo.txt'],
         ];
     }
 }
