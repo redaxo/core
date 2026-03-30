ff751de505e56d57324bc0a848af943dccbe8c72
Gregor Harlan <mail@gh01.de>
2026-03-30 15:00:50 +0200

    fix psalm

--- redaxo/src/addons/structure/plugins/content/fragments/slice_list.php
+++ redaxo/src/addons/structure/plugins/content/fragments/slice_list.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @var rex_fragment $this
- * @psalm-scope-this rex_fragment
- *
  * Fragment variables (via $this->...):
  * - string $content  The rendered HTML of all slice <li> elements
+ *
+ * @var rex_fragment $this
+ * @psalm-scope-this rex_fragment
  */
 if ('' === $this->content) {
     return;
--- redaxo/src/addons/structure/plugins/content/fragments/slice_list_item.php
+++ redaxo/src/addons/structure/plugins/content/fragments/slice_list_item.php
@@ -1,18 +1,16 @@
 <?php
 /**
+ *  Fragment variables (via $this->...):
+ *
+ *  - array<int|string, int|string|list<string>>  $attributes   HTML attributes for the <li> element
+ *  - string  $content  The rendered inner HTML of the slice
+ *  - string|null  $formAction  if set, wraps content in a <form>
+ *
  * @var rex_fragment $this
  * @psalm-scope-this rex_fragment
- *
- * Fragment variables (via $this->...):
- *
- * - array<int|string, int|string|list<string>>  $attributes   HTML attributes for the <li> element
- * - string  $content  The rendered inner HTML of the slice
- * - string|null  $formAction  if set, wraps content in a <form>
  */
-
-$attributes = $this->attributes ?? [];
 ?>
-<li<?= rex_string::buildAttributes($attributes) ?>>
+<li<?= rex_string::buildAttributes($this->attributes ?? []) ?>>
     <?php if (isset($this->formAction) && '' !== $this->formAction): ?>
         <form enctype="multipart/form-data" action="<?= $this->formAction ?>" method="post" id="REX_FORM">
     <?php endif ?>
