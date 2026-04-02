8dd63ae6893ba254c4dfc9814148fbef1b4f423e
Gregor Harlan <mail@gh01.de>
2026-04-02 09:58:15 +0200

    Console setup: possibility to keep existing db password

--- redaxo/src/core/lib/console/setup/run.php
+++ redaxo/src/core/lib/console/setup/run.php
@@ -179,6 +179,10 @@ class rex_command_setup_run extends rex_console_command implements rex_command_o
 
         $io->section('Database information');
 
+        $previousHost = $config['db'][1]['host'] ?? null;
+        $previousLogin = $config['db'][1]['login'] ?? null;
+        $previousPassword = $config['db'][1]['password'] ?? null;
+
         do {
             $config['db'][1]['host'] = $this->getOptionOrAsk(
                 'MySQL Host',
@@ -195,16 +199,28 @@ class rex_command_setup_run extends rex_console_command implements rex_command_o
                 $requiredValue,
             );
 
-            $q = new Question('Password');
-            $q->setHidden(true);
+            $keepPassword = false;
+            if ($previousPassword
+                && $config['db'][1]['host'] === $previousHost
+                && $config['db'][1]['login'] === $previousLogin
+                && null === $input->getOption('db-password')
+                && $input->isInteractive()
+            ) {
+                $keepPassword = $io->confirm('Keep existing database password?', true);
+            }
 
-            $config['db'][1]['password'] = $this->getOptionOrAsk(
-                $q,
-                'db-password',
-                '',
-                'Using database password *secret*',
-                null,
-            );
+            if (!$keepPassword) {
+                $q = new Question('Password');
+                $q->setHidden(true);
+
+                $config['db'][1]['password'] = (string) $this->getOptionOrAsk(
+                    $q,
+                    'db-password',
+                    '',
+                    'Using database password *secret*',
+                    null,
+                );
+            }
 
             $config['db'][1]['name'] = $this->getOptionOrAsk(
                 'Database name',
