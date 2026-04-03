94d4f644c67de9ceab269e373e5a923fbcdade4d
Gregor Harlan <mail@gh01.de>
2026-04-03 12:14:34 +0200

    Debug-Addon: ApiFunction registrieren (#6451)

--- redaxo/src/addons/debug/boot.php
+++ redaxo/src/addons/debug/boot.php
@@ -19,6 +19,8 @@ use Redaxo\Core\Util\Timer;
 
 use function Redaxo\Core\View\escape;
 
+ApiFunction::register('debug', rex_api_debug::class);
+
 if (!rex_debug_clockwork::isRexDebugEnabled() || 'debug' === Request::get(ApiFunction::REQ_CALL_PARAM)) {
     return;
 }
