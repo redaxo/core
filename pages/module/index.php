90859f418bc4fdb0bc1b68c873e7b03bb6ff74fe
Gregor Harlan <mail@gh01.de>
2026-04-12 20:25:45 +0200

    refactor: remove backend pages for template/module/action management
    Templates, modules, and actions are no longer managed via the backend UI.
    They will be defined as PHP classes in the project and addon code instead.

--- pages/module/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Redaxo\Core\Backend\Controller;
-use Redaxo\Core\Translation\I18n;
-use Redaxo\Core\View\View;
-
-echo View::title(I18n::msg('modules'));
-
-Controller::includeCurrentPageSubPath();
